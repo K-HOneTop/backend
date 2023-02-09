84da775053297897ed20c69da8b247f94af7a460
LimGayoung <ygy920@gmail.com>
2023-02-10 01:37:23 +0900

    update: comment

--- src/main/java/khonetop/backend/member/service/EmailService.java
+++ src/main/java/khonetop/backend/member/service/EmailService.java
@@ -2,7 +2,5 @@ package khonetop.backend.member.service;
 
 public interface EmailService {
     void sendMessage(String rcv) throws Exception;
-
-    //emailCode를 redis에 저장해서 code와 사용자 정보 등을 조회해 일치하면 회원가입 되도록 하는 것 추가해주기
     boolean verifyEmailCode(String email, String code);
 }
