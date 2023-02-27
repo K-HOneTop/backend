9d084a27145164c4b2b75d4189ddd47b70e77ba7
LimGayoung <ygy920@gmail.com>
2023-02-28 00:54:55 +0900

    feat: createCertificate service

--- src/main/java/khonetop/backend/Certificate/dto/CreateCertificateRequestDto.java
+++ src/main/java/khonetop/backend/Certificate/dto/CreateCertificateRequestDto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * 자격증 직접 추가 request
@@ -19,5 +21,12 @@ public class CreateCertificateRequestDto {
 
     String org_name;
     String url;
+    Integer validateTerm; //유효기간
+    LocalDateTime startDate; //접수 시작일자
+    LocalDateTime endDate; //접수 종료일자
+    LocalDateTime addStartDate; //추가접수 시작일자
+    LocalDateTime addEndDate; //추가접수 종료일자
+    LocalDate examDate; //시험일자
+    LocalDate announceDate; //성적 발표일자
 
 }
